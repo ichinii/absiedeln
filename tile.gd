739895b051accd71d28a2fd5b1cf93ef46ae28cb
Sven Erdem <svenerdem@gmail.com>
2024-09-22 23:25:29 +0200

    es ist interaktiv

--- tile.gd
+++ tile.gd
@@ -1,8 +1,23 @@
 extends StaticBody2D
 
-
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	
 func _process(delta: float) -> void:
 	pass
+
+func set_color(color: Color):
+	$Polygon2D.color = color
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	var is_click = event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT
+	if is_click:
+		print("is_click")
+
+
+func _on_mouse_entered() -> void:
+	set_color(Color(1, 0, 0))
+
+
+func _on_mouse_exited() -> void:
+	set_color(Color(1, 1, 1))
